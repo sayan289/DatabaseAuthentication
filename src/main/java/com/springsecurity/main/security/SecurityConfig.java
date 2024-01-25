18f800dfe4c4b8eb543098860bf1286e83dc69c1
sayan289 <sayanadhikary289@gmail.com>
2024-01-25 09:54:37 +0530

    Database Authentication

---
+++ src/main/java/com/springsecurity/main/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.springsecurity.main.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.springsecurity.main.dao.UserDetailsRepo;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserDetailsRepo userDetailsRepo) {
+        return new CustomUserDetailsServices(userDetailsRepo);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, DaoAuthenticationProvider authenticationProvider) throws Exception {
+        http.csrf(csrf->csrf.disable())
+    	.authorizeHttpRequests()
+                .requestMatchers("/userdetails/add").permitAll()
+    	.anyRequest().authenticated()
+    	.and()
+    	.httpBasic();
+        return http.build();
+       
+    }
+}
+
---
+++ src/main/java/com/springsecurity/main/services/UserDetailsImpl.java
@@ -0,0 +1,24 @@
+package com.springsecurity.main.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.springsecurity.main.dao.UserDetailsRepo;
+import com.springsecurity.main.entites.UserDe;
+
+@Component
+public class UserDetailsImpl implements UserDetailsService {
+	@Autowired
+	UserDetailsRepo userDetailsRepo;
+	
+	public UserDe addUser(UserDe userDetails) {
+		userDetails.setUserPassword(new BCryptPasswordEncoder().encode(userDetails.getUserPassword()));
+		return userDetailsRepo.save(userDetails);
+		
+	}
+	public Iterable<UserDe> allusers() {
+	return	userDetailsRepo.findAll();
+	}
+	
+}
